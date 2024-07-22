94e602edc57ecfb70fcb8d19c8da17397e08cca7
chirag-codealchemy <chirag.codealchemy@gmail.com>
2024-07-22 16:12:27 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { remotePatterns: [{ hostname: "**", protocol: "https" }] },
-  experimental: { instrumentationHook: JSON.parse(process.env.INSTRUMENTATION_HOOK) },
+  // experimental: { instrumentationHook: JSON.parse(process.env.INSTRUMENTATION_HOOK) },
 };
 
 export default nextConfig;
